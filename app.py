2c218bca6f708212cb7f2276b60390009345b349
Mohammad Ibrahim <Med@dyn-160-39-150-244.dyn.columbia.edu>
2018-09-05 11:13:01 -0400

    did challenge

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests 
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +9,22 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+
+@app.route('/pokemon/<int:id>')
+def getPokemon(id):
+    url = "http://pokeapi.co/api/v2/pokemon/" + str(id)
+    payload = ""
+    response = requests.request("GET", url, data = payload)
+    data = response.json()
+    return "<h1> The pokemon with id " + str(id)+ " is "+data['name'] + "</h1>"
+
+@app.route('/pokemon/<string:name>')
+def getPokemonByName(name):
+    url = "http://pokeapi.co/api/v2/pokemon/" + name
+    payload = ""
+    response = requests.request("GET", url, data = payload)
+    data = response.json()
+    return "<h1> " + name + " has id " + str(data['id']) + "</h1>" 
 
 if __name__ == '__main__':
     app.run()
